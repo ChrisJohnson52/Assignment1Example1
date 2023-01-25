6f2b16ea81a95ddb0bbd8f53852a74b4cdfef15b
Terrordactle <cjoh445@lsu.edu>
2023-01-24 22:47:29 -0600

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+/*
+Christian-Thomas Dougas De Guzman Johnson
+CSC 2463
+Drawing With P5
+*/
+
+//Example 1
+function setup() 
+{
+  createCanvas(400, 200);
+}
+  
+function draw() 
+{
+  background('rgb(0,255,0)');
+  ellipse(100, 100, 160, 160);
+  rect(220, 16, 160, 160);
+}
